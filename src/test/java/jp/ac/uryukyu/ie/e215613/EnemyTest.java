32e1b5825825a15347bd7c5840c43fe3a7373f5e
仲宗根　羽七 <e215613@ie.u-ryukyu.ac.jp>
2023-01-30 18:03:24 +0900

    third commit

--- src/test/java/jp/ac/uryukyu/ie/e215613/EnemyTest.java
+++ src/test/java/jp/ac/uryukyu/ie/e215613/EnemyTest.java
@@ -13,11 +13,18 @@ class EnemyTest {
      * 　　つまり攻撃実行してもヒーローのHPは減っていないことを期待。これを検証する。
      */
     @Test
+    void appHasGreeting(){
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should hava a greeting");
+    }
+    @Test
     void attackTest() {
         int defaultHeroHp = 100;
         Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
         Enemy slime = new Enemy("スライムもどき", 10, 100);
-        demoHero.attack(slime);
+        for(int count=0; count<10; count++){
+            demoHero.attack(slime); 
+        }
         slime.attack(demoHero);
         assertEquals(defaultHeroHp, demoHero.hitPoint);
     }
